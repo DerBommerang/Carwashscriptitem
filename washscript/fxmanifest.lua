8b8a186e0a3b4aea812a6a9a0eb8ac1c95c29639
DerBommerang <patricksanz30@gmail.com>
2024-07-12 23:16:16 +0200

    Update fxmanifest.lua

--- washscript/fxmanifest.lua
+++ washscript/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'DerBommerang'
-description 'Car Wash Script that uses a washcloth item with animation'
+description 'Washscript that uses a washcloth item with animation'
 version '1.0.0'
 
 client_scripts {
